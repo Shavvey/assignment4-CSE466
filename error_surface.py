1e5eb9501d71b86e835017256057e36cb4e248fc
Cole Johnson <coledjohn@gmail.com>
2025-09-29 16:59:44 -0600

    Add plots for first and last half of data points

--- error_surface.py
+++ error_surface.py
@@ -5,14 +5,15 @@ import numpy as np
 from matplotlib import cm
 
 
-def plot_mse(file: str, samples: int, interval: tuple[int, int]):
+def plot_full_mse(file: str, samples: int, interval: tuple[int, int]):
     """Given list of points from file (`data/table1.html` for example),
-    get surface plot of mse given intervals for potential linear regression models"""
+    get full surface plot of mse given intervals for potential linear regression models
+    """
     ms = np.linspace(interval[0], interval[1], samples)
     bs = np.linspace(interval[0], interval[1], samples)
     points = util.get_points(file)
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
+    ax1 = fig.add_subplot(121, projection="3d")
     # create 2D mesh given our sampling points
     X, Y = np.meshgrid(ms, bs)
     Z = np.empty((samples, samples))
@@ -21,7 +22,7 @@ def plot_mse(file: str, samples: int, interval: tuple[int, int]):
         for j, b in enumerate(bs):
             lr = LinearReg(m, b)
             mse = lr.mse(points)
-            error = 20 * np.log10(mse)
+            error = mse
             # find minimum error among tested models
             if min[2] > error:
                 min[0] = m
@@ -29,8 +30,94 @@ def plot_mse(file: str, samples: int, interval: tuple[int, int]):
                 min[2] = error
             Z[j][i] = error
     print(min)
-    ax.plot_surface(X, Y, Z, cmap=cm.get_cmap("coolwarm"))
-    ax.set_xlabel("Slope (m)")
-    ax.set_ylabel("Y-intercept (b)")
-    ax.set_zlabel("Error (mse, log scale)")
+    ax1.plot_surface(X, Y, Z, cmap=cm.get_cmap("coolwarm"))
+    ax1.set_xlabel("Slope (m)")
+    ax1.set_ylabel("Y-intercept (b)")
+    ax1.set_zlabel("Error (mse)")
+    ax2 = fig.add_subplot(122, projection="3d")
+    # apply the log-scale
+    Z = 10 * np.log10(Z)
+    ax2.plot_surface(X, Y, Z, cmap=cm.get_cmap("coolwarm"))
+    ax2.set_xlabel("Slope (m)")
+    ax2.set_ylabel("Y-intercept (b)")
+    ax2.set_zlabel("Error (mse, log scale)")
+    plt.show()
+
+
+def plot_fhalf_mse(file: str, samples: int, interval: tuple[int, int]):
+    """Given list of points from file (`data/table1.html` for example),
+    get surface plot of mse given intervals for potential linear regression models"""
+    ms = np.linspace(interval[0], interval[1], samples)
+    bs = np.linspace(interval[0], interval[1], samples)
+    points = util.get_points(file)
+    plen = len(points)
+    points = points[:plen//2]
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121, projection="3d")
+    # create 2D mesh given our sampling points
+    X, Y = np.meshgrid(ms, bs)
+    Z = np.empty((samples, samples))
+    min = [X[0], Y[0], 1 << 32]
+    for i, m in enumerate(ms):
+        for j, b in enumerate(bs):
+            lr = LinearReg(m, b)
+            mse = lr.mse(points)
+            error = mse
+            # find minimum error among tested models
+            if min[2] > error:
+                min[0] = m
+                min[1] = b
+                min[2] = error
+            Z[j][i] = error
+    print(min)
+    ax1.plot_surface(X, Y, Z, cmap=cm.get_cmap("coolwarm"))
+    ax1.set_xlabel("Slope (m)")
+    ax1.set_ylabel("Y-intercept (b)")
+    ax1.set_zlabel("Error (mse)")
+    ax2 = fig.add_subplot(122, projection="3d")
+    # apply the log-scale
+    Z = 10 * np.log10(Z)
+    ax2.plot_surface(X, Y, Z, cmap=cm.get_cmap("coolwarm"))
+    ax2.set_xlabel("Slope (m)")
+    ax2.set_ylabel("Y-intercept (b)")
+    ax2.set_zlabel("Error (mse, log scale)")
+    plt.show()
+
+def plot_lhalf_mse(file: str, samples: int, interval: tuple[int, int]):
+    """Given list of points from file (`data/table1.html` for example),
+    get surface plot of mse given intervals for potential linear regression models"""
+    ms = np.linspace(interval[0], interval[1], samples)
+    bs = np.linspace(interval[0], interval[1], samples)
+    points = util.get_points(file)
+    plen = len(points)
+    points = points[plen//2:]
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121, projection="3d")
+    # create 2D mesh given our sampling points
+    X, Y = np.meshgrid(ms, bs)
+    Z = np.empty((samples, samples))
+    min = [X[0], Y[0], 1 << 32]
+    for i, m in enumerate(ms):
+        for j, b in enumerate(bs):
+            lr = LinearReg(m, b)
+            mse = lr.mse(points)
+            error = mse
+            # find minimum error among tested models
+            if min[2] > error:
+                min[0] = m
+                min[1] = b
+                min[2] = error
+            Z[j][i] = error
+    print(min)
+    ax1.plot_surface(X, Y, Z, cmap=cm.get_cmap("coolwarm"))
+    ax1.set_xlabel("Slope (m)")
+    ax1.set_ylabel("Y-intercept (b)")
+    ax1.set_zlabel("Error (mse)")
+    ax2 = fig.add_subplot(122, projection="3d")
+    # apply the log-scale
+    Z = 10*np.log10(Z)
+    ax2.plot_surface(X, Y, Z, cmap=cm.get_cmap("coolwarm"))
+    ax2.set_xlabel("Slope (m)")
+    ax2.set_ylabel("Y-intercept (b)")
+    ax2.set_zlabel("Error (mse, log scale)")
     plt.show()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import error_surface as es
 import util as util
 
 def main():
-    es.plot_mse(util.DATA_FILES[0], 1000, (-5,5))
+    es.plot_full_mse(util.DATA_FILES[0], 1000, (-5,5))
     
     
 if __name__ == "__main__":
